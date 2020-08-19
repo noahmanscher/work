1af6d6dae28d1cca5a82599c3f37a9d261655944
Noah Manscher <noahmanscher@gmail.com>
2020-08-19 17:30:36 +0200

    upload animasher build

---
+++ animasher/precache-manifest.75ae2ca222b23d16ba20eedbaadd2199.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6043e4578bccbbf1c8ba2d4120c8cc22",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fc1c80e8fd340e6d08b3",
+    "url": "./static/css/main.412a8193.chunk.css"
+  },
+  {
+    "revision": "983e7b8b44ed12bfc317",
+    "url": "./static/js/2.67bcd69d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.67bcd69d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc1c80e8fd340e6d08b3",
+    "url": "./static/js/main.ef0103df.chunk.js"
+  },
+  {
+    "revision": "1d61a8b21c20c056169b",
+    "url": "./static/js/runtime-main.1af2f402.js"
+  },
+  {
+    "revision": "c8fbf61dd096c583a4d0fe60f294d95c",
+    "url": "./static/media/fpvid.c8fbf61d.mp4"
+  },
+  {
+    "revision": "cfa8d3f6e2322011d87d1ed684e48077",
+    "url": "./static/media/logo-blur.cfa8d3f6.png"
+  }
+]);
